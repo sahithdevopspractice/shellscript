9ccf26c92dff9e9bb0a5afe48ad532801233e4ec
sahithdevopspractice <sahithdevopspractice@gmail.com>
2024-06-24 09:32:05 -0400

    shell script

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,7 +3,7 @@
 echo "Enter your first number"
 NUMBER=$1 #take it from the user as an argument
 
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
     echo "Given Number $NUMBER is greater than 100"
 else
