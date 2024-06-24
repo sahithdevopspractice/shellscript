41875f9fc5285193a177832fcf305bce3a046535
sahithdevopspractice <sahithdevopspractice@gmail.com>
2024-06-23 22:58:23 -0400

    shell script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh Good Morning"
+echo "Suresh:: Hi Ramesh Very Good Morning"
+echo "Ramesh:: How are you suresh?"
+echo "Suresh:: Am good how are you ramesh"
