ec923ce565c4d87fa2b6b4e4253f4e5949399314
sahithdevopspractice <sahithdevopspractice@gmail.com>
2024-06-24 11:30:00 -0400

    shell script

--- 10-Functions.sh
+++ 10-Functions.sh
@@ -2,14 +2,23 @@
 
 ID=$(id -u)
 
+# VALIDATE(){
+#     if [ $? -ne 0 ]
+#         then 
+#             echo "ERROR:: Installing  is Failed"
+#             exit 1
+#         else 
+#             echo "Installing  is Success"
+#     fi
+# }
+ 
 VALIDATE(){
-    if [ $? -ne 0 ]
-        then 
-            echo "ERROR:: Installing  is Failed"
-            exit 1
-        else 
-            echo "Installing  is Success"
-    fi
+if [$1 -ne 0]
+    then
+    echo "ERROR:: $2... FAILED"
+    exit 1
+else
+    echo "$2.... SUCCESS"
 }
 
 if [ $ID -ne 0 ]
@@ -20,7 +29,12 @@ else
     echo "You are root user"
 fi
 
+# yum install mysql -y
+# VALIDATE
+# yum install git -y
+# VALIDATE
+
 yum install mysql -y
-VALIDATE
+VALIDATE $? "Installing MySQL"
 yum install git -y
-VALIDATE
+VALIDATE $? "Installing GIT"
