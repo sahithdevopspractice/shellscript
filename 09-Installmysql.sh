4963777a02f4951b5116bead3d3e1969c8556e8c
sahithdevopspractice <sahithdevopspractice@gmail.com>
2024-06-24 10:55:32 -0400

    shell script

--- 09-Installmysql.sh
+++ 09-Installmysql.sh
@@ -10,3 +10,11 @@ else
     echo "You are root user"
 fi
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR:: Installing Msql is Failed"
+    exit 1
+else 
+    echo "Installing MYSql is Success"
+fi
